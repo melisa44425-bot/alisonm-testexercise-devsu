57abff52964e7ea474698b69fd783b16a63c5b24
Alison Martin <melisa.44425@gmail.com>
2026-02-11 22:36:20 -0500

    Fix specPattern path case

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,10 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    specPattern: [
+      "cypress/e2e/**/*.cy.js",
+      "cypress/api/**/*.cy.js",
+
+  ],
   },
 });
